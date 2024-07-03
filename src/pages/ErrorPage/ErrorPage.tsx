057bfcbf2df16ec46d073939df417959519eb2cb
Katherine <f.katttherine@gmail.com>
2024-07-03 15:02:00 +0300

    +errorPage

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,3 +1,17 @@
+import styles from "./ErrorPage.module.css";
+
 export function ErrorPage() {
-  return <>Errorrrrrrr</>;
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles["error-content"]}>
+          <img src="/src/assets/images/illustration.png" alt="404" />
+          <span className={styles.head}>Page not found</span>
+          <span className={styles.message}>
+            Sorry, we can’t find what you’re looking for
+          </span>
+        </div>
+      </div>
+    </>
+  );
 }
