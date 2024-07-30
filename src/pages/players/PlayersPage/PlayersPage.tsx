e914693012e2c79070c35677fb50538fd68c8f3a
Katherine <f.katttherine@gmail.com>
2024-07-30 16:56:31 +0300

    + search in Players, filter by teams, pagination, grid in Players

--- src/pages/players/PlayersPage/PlayersPage.tsx
+++ src/pages/players/PlayersPage/PlayersPage.tsx
@@ -9,78 +9,148 @@ import CustomSelect, {
 import styles from "./PlayersPage.module.css";
 import PlayerCard from "../components/PlayerCard/PlayerCard";
 import { fetchPlayersThunk } from "../../../core/redux/playersThunks/fetchPlayersThunk";
+import { fetchTeams } from "../../../core/redux/teamsThunks/fetchTeamsThunk";
 import { useNavigate } from "react-router-dom";
 import cn from "classnames";
+import ReactPaginate from "react-paginate";
+import PageSelect from "../../../ui/PageSelect/PageSelect";
 
 export const PlayersPage: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const { data: players, status } = useSelector(
-    (state: RootState) => state.players
-  );
-
+  const {
+    data: players,
+    count: totalPlayers,
+    status: playersStatus,
+  } = useSelector((state: RootState) => state.players);
+  const { data: teams } = useSelector((state: RootState) => state.teams);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    dispatch(fetchPlayersThunk({ name: "", page: 1, pageSize: 6 }));
-  }, [dispatch]);
-
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(6);
+  const [searchQuery, setSearchQuery] = useState("");
   const [value, setValue] = useState<SelectOption[]>([]);
 
-  const playerOptions: SelectOption[] = players.map((player) => ({
-    name: player.name,
-    id: player.id,
+  useEffect(() => {
+    dispatch(fetchPlayersThunk({ name: searchQuery, page, pageSize }));
+    dispatch(fetchTeams({ name: "", page: 1, pageSize: 100 }));
+  }, [dispatch, page, pageSize, searchQuery]);
+
+  const teamOptions: SelectOption[] = teams.map((team) => ({
+    name: team.name,
+    id: team.id,
   }));
 
   const handleCardClick = (id: number) => {
     navigate(`/players/${id}`);
   };
 
-  const handleSearchChange = (query: string) => {};
+  const handlePageChange = ({ selected }: { selected: number }) => {
+    setPage(selected + 1);
+  };
+
+  const handlePageSizeChange = (newPageSize: number) => {
+    setPageSize(newPageSize);
+    setPage(1);
+  };
+
+  const handleSearch = (query: string) => {
+    setSearchQuery(query.toLowerCase().trim());
+    setPage(1);
+  };
+
+  const getGridStyle = () => {
+    if (pageSize === 6) return styles.six;
+    if (pageSize === 12) return styles.twelve;
+    return styles.twentyfour;
+  };
+
+  const filteredPlayers = players.filter((player) =>
+    value.length === 0 ? true : value.some((team) => team.id === player.team)
+  );
 
   return (
-    <div className={cn(styles.container)}>
-      <main className={styles.main}>
-        <div className={styles.head}>
-          <Search onSearch={handleSearchChange} />
-          <CustomSelect
-            appearance="multi"
-            options={playerOptions}
-            value={value}
-            placeholder="Select..."
-            onChange={setValue}
-          />
-          <Button
-            appearence="add"
-            onClick={() => navigate("/players/add_new_player")}
+    <>
+      <div className={cn(styles.container)}>
+        <main className={styles.main}>
+          <div className={styles.head}>
+            <Search onSearch={handleSearch} />
+            <CustomSelect
+              appearance="multi"
+              options={teamOptions}
+              value={value}
+              placeholder="Select..."
+              onChange={setValue}
+            />
+            <Button
+              appearence="add"
+              onClick={() => navigate("/players/add_new_player")}
+            >
+              Add +
+            </Button>
+          </div>
+
+          <div
+            className={cn(
+              styles.cards,
+              getGridStyle(),
+              filteredPlayers.length === 0 ? styles.empty : ""
+            )}
           >
-            Add +
-          </Button>
-        </div>
-        <div
-          className={cn(styles.cards, players.length === 0 ? styles.empty : "")}
-        >
-          {status === "loading" && <p>Loading...</p>}
-          {status === "succeeded" &&
-            players.map((player) => (
-              <PlayerCard
-                key={player.id}
-                player={player}
-                onClick={() => handleCardClick(player.id)}
-              />
-            ))}
-          {status === "succeeded" && players.length === 0 && (
-            <div className={styles.emptyPage}>
-              <img
-                src="/src/assets/images/emptyPlayers.png"
-                alt="Play BasketBall)"
-              />
-              <div className={styles.headText}>Empty here</div>
-              <div className={styles.text}>Add new teams to continue</div>
+            {playersStatus === "loading" && <p>Loading...</p>}
+            {playersStatus === "succeeded" &&
+              filteredPlayers.map((player) => (
+                <PlayerCard
+                  key={player.id}
+                  player={player}
+                  onClick={() => handleCardClick(player.id)}
+                />
+              ))}
+            {playersStatus === "succeeded" && filteredPlayers.length === 0 && (
+              <div className={styles.emptyPage}>
+                <img
+                  src="/src/assets/images/emptyPlayers.png"
+                  alt="Play BasketBall"
+                />
+                <div className={styles.headText}>Empty here</div>
+                <div className={styles.text}>Add new teams to continue</div>
+              </div>
+            )}
+            {playersStatus === "failed" && <p>Failed to load players</p>}
+          </div>
+
+          <div className={styles["pagination-container"]}>
+            <ReactPaginate
+              previousLabel={
+                <img
+                  src="/src/assets/icon/chevron_left_24px.svg"
+                  alt="Previous Page"
+                />
+              }
+              nextLabel={
+                <img
+                  src="/src/assets/icon/chevron_right_24px.svg"
+                  alt="Next Page"
+                />
+              }
+              breakLabel={"..."}
+              pageCount={Math.ceil(totalPlayers / pageSize)}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={5}
+              onPageChange={handlePageChange}
+              className={styles.pagination}
+              activeClassName={styles.active}
+            />
+            <div className={styles.controls}>
+              <label>
+                <PageSelect
+                  options={[6, 12, 24]}
+                  value={pageSize}
+                  onChange={handlePageSizeChange}
+                />
+              </label>
             </div>
-          )}
-          {status === "failed" && <p>Failed to load players</p>}
-        </div>
-      </main>
-    </div>
+          </div>
+        </main>
+      </div>
+    </>
   );
 };
